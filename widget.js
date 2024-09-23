be796d5ec53fa7ce9a23f17dfab82e88f601938a
JakubB <135221834+jakubB6@users.noreply.github.com>
2024-09-23 17:10:22 +0200

    second commit

--- widget.js
+++ widget.js
@@ -1,13 +1,15 @@
 (function () {
+  const scriptTag = document.currentScript;
+  const botID = scriptTag.id.replace(/\s/g, "");
+  const bubbleColor = scriptTag.getAttribute('data-color') || "royalblue"; // Default color if not specified
+
   const SIZE = 60;
   const BTN_RAD = SIZE / 2;
-  const BG_CHAT = "royalblue";
 
   const chatButtonLogo = `
   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="white" stroke-width="1" stroke="white">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
 </svg>
-
 `;
 
   const chevronDownLogo = `
@@ -19,9 +21,9 @@
   const style = document.createElement("style");
   style.innerHTML = `
   @keyframes pulse {
-    0% { box-shadow: 0 0 0 0 rgba(65, 105, 225, 0.7); }
-    70% { box-shadow: 0 0 0 10px rgba(65, 105, 225, 0); }
-    100% { box-shadow: 0 0 0 0 rgba(65, 105, 225, 0); }
+    0% { box-shadow: 0 0 0 0 ${bubbleColor}B3; }
+    70% { box-shadow: 0 0 0 10px ${bubbleColor}00; }
+    100% { box-shadow: 0 0 0 0 ${bubbleColor}00; }
   }
 
   @keyframes pullUp {
@@ -74,7 +76,7 @@
   chatButton.style.width = SIZE + "px";
   chatButton.style.height = SIZE + "px";
   chatButton.style.borderRadius = BTN_RAD + "px";
-  chatButton.style.backgroundColor = BG_CHAT;
+  chatButton.style.backgroundColor = bubbleColor;
   chatButton.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
   chatButton.style.cursor = "pointer";
   chatButton.style.zIndex = 999999999;
@@ -109,9 +111,6 @@
   chat.style.visibility = "hidden";
   chat.style.display = "none";
 
-  const scriptTag = document.currentScript;
-  const botID = scriptTag.id.replace(/\s/g, "");
-
   console.log(botID);
   const iframe = document.createElement("iframe");
   iframe.src = `http://localhost:3000/chat/${botID}`;
@@ -121,8 +120,6 @@
   iframe.style.overflow = "hidden";
   iframe.sandbox = "allow-same-origin allow-scripts allow-popups allow-forms";
   chat.appendChild(iframe);
-
-  // Add this after creating the iframe
 
   document.body.appendChild(chatButton);
   document.body.appendChild(chat);
